da51dd66ca0d6eaaa7a677bca0f5fdec0f0511fc
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-12 16:47:57 -0600

    26. Tarea y Resolución del Ejercicio #1

--- bases/app.js
+++ bases/app.js
@@ -1,5 +1,35 @@
 "use strict";
 (() => {
-    const a = 10;
-    console.log(a);
+    // Tipos
+    const batman = "Bruce";
+    const superman = "Clark";
+    const existe = false;
+    // Tuplas
+    const parejaHeroes = [batman, superman];
+    const villano = ["Lex Lutor", 5, true];
+    // Arreglos
+    const aliados = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+    //Enumeraciones
+    let Power;
+    (function (Power) {
+        Power[Power["min"] = 0] = "min";
+        Power[Power["middle"] = 1] = "middle";
+        Power[Power["max"] = 5] = "max";
+        Power[Power["super"] = 100] = "super";
+    })(Power || (Power = {}));
+    const fuerzaFlash = Power.max;
+    const fuerzaSuperman = Power.super;
+    const fuerzaBatman = Power.middle;
+    const fuerzaAcuaman = Power.min;
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return "activada";
+    }
+    function pedir_ayuda() {
+        console.log("Auxilio!!!");
+    }
+    // Aserciones de Tipo
+    const poder = "100";
+    const largoDelPoder = poder.length;
+    console.log(largoDelPoder);
 })();
--- bases/app.ts
+++ bases/app.ts
@@ -1,4 +1,40 @@
 (() => {
-  const a: number = 10;
-  console.log(a);
+  // Tipos
+  const batman: string = "Bruce";
+  const superman: string = "Clark";
+
+  const existe: boolean = false;
+
+  // Tuplas
+  const parejaHeroes: [string, string] = [batman, superman];
+  const villano: [string, number, boolean] = ["Lex Lutor", 5, true];
+
+  // Arreglos
+  const aliados: string[] = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+
+  //Enumeraciones
+  enum Power {
+    min = 0,
+    middle = 1,
+    max = 5,
+    super = 100,
+  }
+  const fuerzaFlash: Power = Power.max;
+  const fuerzaSuperman: Power = Power.super;
+  const fuerzaBatman: Power = Power.middle;
+  const fuerzaAcuaman: Power = Power.min;
+
+  // Retorno de funciones
+  function activar_batiseñal(): string {
+    return "activada";
+  }
+
+  function pedir_ayuda(): void {
+    console.log("Auxilio!!!");
+  }
+
+  // Aserciones de Tipo
+  const poder: any = "100";
+  const largoDelPoder: number = (poder as string).length;
+  console.log(largoDelPoder);
 })();
