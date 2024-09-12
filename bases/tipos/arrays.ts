cb5a369cdb1ebc44c586ca376a423661aa18057c
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-12 15:06:25 -0600

    19. Arrays - Arreglos

---
+++ bases/tipos/arrays.ts
@@ -0,0 +1,8 @@
+(() => {
+  // const numbers: (string | number | boolean)[] = ...;
+  const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const villains = ["Omega Red", "Red Skull", "Thanos"];
+
+  villains.forEach((v) => console.log(v.toUpperCase()));
+})();
