f0f93cdcd622f8c3937dced3314e766e72c2ace7
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-13 21:25:20 -0600

    30. Parámetros opcionales de las funciones

---
+++ bases/funciones/args-optional.ts
@@ -0,0 +1,9 @@
+(() => {
+  const fullName = (firstName: string, lastName?: string): string => {
+    return `${firstName} ${lastName || "No last name"}`;
+  };
+
+  const name = fullName("Clark");
+
+  console.log({ name });
+})();
