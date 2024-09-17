f772b8fa5dd4974c703881ecf0bf95ff3cf1f981
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-17 15:23:15 -0600

    52. Variables LET

---
+++ bases/ES6/let-var.ts
@@ -0,0 +1,12 @@
+(() => {
+  // console.log(a);
+  const nombre: string = "Emmanuel";
+
+  const getName = (): void => {
+    console.log("viejo getName");
+  };
+
+  // getName = () => { console.log('nuevo getName'); }
+
+  // getName();
+})();
