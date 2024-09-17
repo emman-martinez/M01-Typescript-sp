3625eede13a8537824c612976be40919bb645b29
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-17 12:58:55 -0600

    38. Métodos dentro de los objetos

--- bases/objetos/objects.js
+++ bases/objetos/objects.js
@@ -9,9 +9,9 @@
         name: "Clark Kent",
         // age: 60,
         powers: ["Super fuerza"],
-        // getName() {
-        //   return this.name;
-        // },
+        getName() {
+            return this.name;
+        },
     };
-    console.log(flash);
+    console.log(flash.getName());
 })();
