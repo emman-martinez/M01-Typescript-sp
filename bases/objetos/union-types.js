297125e6cb9ca2d69a9a8605af65ec8407a622da
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-17 13:19:13 -0600

    41. Multiples tipos permitidos

---
+++ bases/objetos/union-types.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let myCustomVariable = "Emmanuel";
+    console.log(typeof myCustomVariable);
+    myCustomVariable = 20;
+    console.log(typeof myCustomVariable);
+    myCustomVariable = {
+        name: "Emmanuel",
+        age: 20,
+        powers: [1, 2, 3],
+    };
+    console.log(typeof myCustomVariable);
+})();
