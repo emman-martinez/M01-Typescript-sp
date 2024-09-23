55bbfcc96b69ea3da289cab55651ec19664875b6
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-23 15:53:59 -0600

    61. Métodos públicos y privados

--- bases/classes/basica.ts
+++ bases/classes/basica.ts
@@ -4,15 +4,23 @@
     // private team: string;
     // public realName?: string;
     static avgAge: number = 35;
+    static getAvgAge() {
+      return this.name;
+    }
 
     constructor(
       private name: string,
       private team: string,
       public realName?: string
     ) {}
+
+    public bio(): string {
+      return `${this.name} (${this.team})`;
+    }
   }
 
   const antman: Avenger = new Avenger("Antman", "Captain", "Scott Lang");
   console.log({ antman });
-  console.log(Avenger.avgAge);
+  console.log(antman.bio());
+  console.log(Avenger.getAvgAge());
 })();
--- bases/main.js
+++ bases/main.js
@@ -1,15 +1,22 @@
 "use strict";
 (() => {
     class Avenger {
+        static getAvgAge() {
+            return this.name;
+        }
         constructor(name, team, realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger("Antman", "Captain", "Scott Lang");
     console.log({ antman });
-    console.log(Avenger.avgAge);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
 })();
 //# sourceMappingURL=main.js.map
