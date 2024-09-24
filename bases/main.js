cde6eebede509677932a5703622dbe4c4abe7a1b
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-24 17:20:15 -0600

    77. Creando un Namespace

--- bases/main.js
+++ bases/main.js
@@ -1,64 +1,12 @@
 "use strict";
-(() => {
-    let Power;
-    (function (Power) {
-        Power[Power["SUPER_SPEED"] = 0] = "SUPER_SPEED";
-        Power[Power["TIME_TRAVEL"] = 1] = "TIME_TRAVEL";
-    })(Power || (Power = {}));
-    let flash = {
-        name: "Barry Allen",
-        age: 24,
-        powers: [Power.SUPER_SPEED, Power.TIME_TRAVEL],
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return text.length > 3;
     };
-    let superman = {
-        name: "Clark Kent",
-        age: 60,
-        powers: [Power.SUPER_SPEED],
-        getName() {
-            return this.name;
-        },
+    Validations.validateDate = (myDate) => {
+        return isNaN(myDate.valueOf()) ? false : true;
     };
-})();
-(() => {
-    class Mutant {
-        constructor(age, name, realName) {
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-        }
-        mutantPower(id) {
-            return this.name + " " + this.realName;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: "Emmanuel",
-        age: 34,
-        address: {
-            id: 125,
-            zip: "KY2 SUD",
-            city: "Ottawa",
-        },
-        getFullAddress(id) {
-            return `${this.address.zip} - ${this.address.city}`;
-        },
-    };
-    const client2 = {
-        name: "Damaris",
-        age: 37,
-        address: {
-            id: 126,
-            zip: "KY2 SUD",
-            city: "Ottawa",
-        },
-        getFullAddress(id) {
-            return `${this.address.zip} - ${this.address.city}`;
-        },
-    };
-})();
-(() => {
-    let addNumbersFunction;
-    addNumbersFunction = (a, b) => a + b;
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText("Emmanuel"));
 //# sourceMappingURL=main.js.map
