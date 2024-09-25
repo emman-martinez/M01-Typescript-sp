8b20768d954ce8c2e3f786fa1976882f9b4425e2
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-24 20:15:54 -0600

    81. Tarea - Resolver errores en TypeScrip

--- ts-node/src/classes/Hero.ts
+++ ts-node/src/classes/Hero.ts
@@ -1,7 +1,15 @@
+import powers from "../data/power";
+
 export class Hero {
   constructor(
     public name: string,
     public powerId: number,
     public age: number
   ) {}
+
+  get power(): string {
+    return (
+      powers.find((power) => power.id === this.powerId)?.desc || "not found"
+    );
+  }
 }
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,8 +1,10 @@
-// import { Hero as SuperHero } from "./classes/Hero";
-import * as HeroClasses from "./classes/Hero";
-import powers from "./data/power";
+import { Hero } from "./classes/Hero";
+//import { Hero as SuperHero } from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero";
+// import powers from "./data/power";
 
 // const ironman = new SuperHero("Ironman", 1, 55);
-const ironman = new HeroClasses.Hero("Ironman", 1, 55);
+const ironman = new Hero("Ironman", 1, 55);
 console.log(ironman);
-console.log(powers);
+console.log(ironman.power);
+// console.log(powers);
