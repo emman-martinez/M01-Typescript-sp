05cb27098a7c678b26a5501f70b601540705261a
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-27 16:01:39 -0600

    86. Ejemplo de función genérica en acción

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -3,6 +3,8 @@ import {
   genericFunction,
   genericFunctionArrow,
 } from "./generics/generics";
+import { Hero } from "./interfaces/hero";
+import { Villain } from "./interfaces/villain";
 
 // printObject(123);
 // printObject(new Date());
@@ -10,6 +12,14 @@ import {
 // printObject([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 // printObject("Hello, world!");
 
-console.log(genericFunctionArrow(3.1416).toFixed(2));
-console.log(genericFunctionArrow("Hello World!").toUpperCase());
-console.log(genericFunctionArrow(new Date()).getDate());
+// console.log(genericFunctionArrow(3.1416).toFixed(2));
+// console.log(genericFunctionArrow("Hello World!").toUpperCase());
+// console.log(genericFunctionArrow(new Date()).getDate());
+
+const deadpool = {
+  name: "Deadpool",
+  realName: "Wade Wilson",
+  dangerLevel: 130,
+};
+
+console.log(genericFunctionArrow<Villain>(deadpool).dangerLevel);
