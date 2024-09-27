8f3b803642c0fda283ac1610eeb9d45459650ce2
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-27 15:45:10 -0600

    84. Introducción a los Genéricos

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,10 +1,10 @@
-import { Hero } from "./classes/Hero";
-//import { Hero as SuperHero } from "./classes/Hero";
-// import * as HeroClasses from "./classes/Hero";
-// import powers from "./data/power";
+import { printObject, genericFunction } from "./generics/generics";
 
-// const ironman = new SuperHero("Ironman", 1, 55);
-const ironman = new Hero("Ironman", 1, 55);
-console.log(ironman);
-console.log(ironman.power);
-// console.log(powers);
+// printObject(123);
+// printObject(new Date());
+// printObject({ a: 1, b: 2, c: 3 });
+// printObject([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// printObject("Hello, world!");
+
+console.log(genericFunction(3.1416).toFixed(2));
+console.log(genericFunction(new Date()).toFixed(2));
