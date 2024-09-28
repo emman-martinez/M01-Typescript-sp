697263a9828b2d43c6ac7859685eed86133c67ab
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-27 21:12:41 -0600

    90. Quicktype.io extensión

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,6 +1,6 @@
 import { getPokemon } from "./generics/get-pokemon";
 
 getPokemon(4)
-  .then((pokemon) => console.log(pokemon.sprites.front_default))
+  .then((pokemon) => console.log(pokemon))
   .catch((err) => console.error(err))
   .finally(() => console.log("Finally block"));
