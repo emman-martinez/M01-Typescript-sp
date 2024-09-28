7dafb37032014f86c1a759e9b22c2867e1281833
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-27 21:23:46 -0600

    93. Introducción a los decoradores

---
+++ ts-node/src/backs/quicktype-io.ts
@@ -0,0 +1,6 @@
+import { getPokemon } from "../generics/get-pokemon";
+
+getPokemon(4)
+  .then((pokemon) => console.log(pokemon))
+  .catch((err) => console.error(err))
+  .finally(() => console.log("Finally block"));
--- ts-node/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { getPokemon } from "./generics/get-pokemon";
-
-getPokemon(4)
-  .then((pokemon) => console.log(pokemon))
-  .catch((err) => console.error(err))
-  .finally(() => console.log("Finally block"));
