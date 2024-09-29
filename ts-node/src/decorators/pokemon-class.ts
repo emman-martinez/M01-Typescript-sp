0ff670a4de31ee509330687489901d25956d21f3
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-29 16:04:39 -0600

    97. Decoradores de métodos

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -11,9 +11,32 @@ const bloquearPrototipo = function (constructor: Function) {
   Object.seal(constructor.prototype);
 };
 
+function CheckValidPokemonId() {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = (id: number) => {
+      if (id < 1 || id > 800) {
+        return console.error(`Pokemon id has to be between 1 and 800`);
+      } else {
+        return originalMethod(id);
+      }
+    };
+  };
+}
+
 @bloquearPrototipo
-@printToConsoleConditional(true)
+@printToConsoleConditional(false)
 export class Pokemon {
   public publicApi: string = "https://pokeapi.co";
   constructor(public name: string) {}
+
+  @CheckValidPokemonId()
+  savePokemonToDB(id: number) {
+    console.log(`Saved Pokemon into DB.. ${id}`);
+  }
 }
