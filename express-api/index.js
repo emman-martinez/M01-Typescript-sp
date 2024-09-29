936b677e38c1dd42b1386330ea42cef658939eb3
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-29 16:42:40 -0600

    102. Creando un Rest API con Express

--- express-api/index.js
+++ express-api/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+var express = require("express");
+var app = express();
+var port = 3000;
+app.get("/", function (req, res) {
+    res.json({
+        ok: true,
+        msg: "All good",
+    });
+});
+app.listen(port, function () {
+    console.log("Example app listening on port ".concat(port));
+});
