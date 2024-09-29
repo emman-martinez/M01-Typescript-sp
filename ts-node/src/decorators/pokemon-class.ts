39a2d165da0a1c49e7b83df4aa29946c6f235289
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2024-09-29 15:34:48 -0600

    95. Decoradores de fabrica - Factory decorators

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -2,7 +2,11 @@ function printToConsole(constructor: Function) {
   console.log(constructor);
 }
 
-@printToConsole
+const printToConsoleConditional = (print: boolean = false): Function => {
+  return print ? printToConsole : () => {};
+};
+
+@printToConsoleConditional(true)
 export class Pokemon {
   public publicApi: string = "https://pokeapi.co";
   constructor(public name: string) {}
